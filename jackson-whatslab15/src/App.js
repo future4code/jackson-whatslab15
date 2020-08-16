76e17ac197d6a921103254102d2bfe3716d95dd9
unknown <tbrito495@gmail.com>
2020-08-16 13:48:27 -0700

    Projeto finalizado, sem implementaçoes dos desafios

--- jackson-whatslab15/src/App.js
+++ jackson-whatslab15/src/App.js
@@ -1,13 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import styled from 'styled-components';
+const Container =  styled.div`
+display:flex;
+height:90vh;
+max-width:700px;
+align-items:center;
+align-self:center;
+border:1px solid black;
+flex-direction:column-reverse;
+`
+const ContainerMensagens = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 20px;
+`
 
-function App() {
-  return (
-    <div className="App">
+const Mensagens = styled.div`
+  display: flex;
+  flex-direction:row;
+`
+const Bold = styled.span`
+display:inline;
+font-weight: bold;
+text-align:left;
+`
 
-    </div>
-  );
+class App extends React.Component {
+
+
+  state = {
+
+    valorMensagem: [
+
+      // {
+      //   nomeUsuario:"",
+      //   textoMensagem:""
+      // },
+      
+    ],
+
+        inputNomeUsuario:"",
+        inputTextoMensagem:""
+  };
+
+ novamensagem =() =>{
+
+
+  console.log("ola mundo")
+
+  const valores = {
+
+    nomeUsuario: this.state.inputNomeUsuario,
+    textoMensagem: this.state.inputTextoMensagem
+
+  };
+  
+
+  const novosValores = [...this.state.valorMensagem, valores];
+
+  this.setState({valorMensagem: novosValores, inputTextoMensagem: ""});
+  };
+
+  onChangeinputNomeUsuario = (event) =>{
+    this.setState({inputNomeUsuario: event.target.value});
+  };
+
+  onChangeinputTextoMensagem = (event) =>{ 
+    this.setState({inputTextoMensagem: event.target.value});
+  }
+
+
+  render(){
+
+    const mensagens = this.state.valorMensagem.map((valorMensagem) =>{
+
+      return(
+
+        <div>
+          <p>
+          <span><Bold>{valorMensagem.nomeUsuario}</Bold></span>
+          <span>{": " + valorMensagem.textoMensagem}</span>
+          </p>
+        </div>
+
+      
+
+      )
+
+     })
+     
+    return (
+    <ContainerMensagens>
+      <Container>
+        <div>
+          <Mensagens><div>{mensagens}</div></Mensagens>
+            <input 
+            type="text" placeholder={"Usuário"} onChange={this.onChangeinputNomeUsuario}
+             
+      
+            />
+
+            <input type="text" placeholder={"Mensagem"} onChange={this.onChangeinputTextoMensagem } value={this.state.inputTextoMensagem}
+      
+            />
+
+          <button onClick={this.novamensagem}>Enviar</button>
+  
+        </div>
+      </Container>
+    </ContainerMensagens> 
+    
+    );
+  }  
 }
 
 export default App;
